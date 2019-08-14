9a6477856406e9e8eded2ad7e12d2b40f8cbfa4d
Lee <gh243700@gmail.com>
2019-08-14 15:51:56 +0900

    2019-8-14 firstCommit

--- src/main/java/com/sample/config/JdbcConfig.java
+++ src/main/java/com/sample/config/JdbcConfig.java
@@ -1,8 +1,11 @@
 package com.sample.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @PropertySource(value = "classpath:database/jdbc.properties")
@@ -13,10 +16,25 @@ public class JdbcConfig {
   private String className;
   @Value(value = "${jdbc.url}")
   private String url;
-  @Value()
+  @Value(value = "${jdbc.username}")
+  private String username;
+  @Value(value = "${jdbc.password}")
+  private String password;
 
+  @Bean
+  public BasicDataSource basicDataSource(){
+    BasicDataSource basicDataSource = new BasicDataSource();
+    basicDataSource.setDriverClassName(className);
+    basicDataSource.setUrl(url);
+    basicDataSource.setUsername(username);
+    basicDataSource.setPassword(password);
+    return basicDataSource;
+  }
 
+  @Bean
+  public JdbcTemplate jdbcTemplate() {
+    return new JdbcTemplate(basicDataSource());
+  }
 
-
-
+  
 }
